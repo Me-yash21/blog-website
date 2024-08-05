1e9ff0ee5701acd54df55023cb705db06991d567
Yash Saini <yash@yashsaini.com>
2024-08-05 21:19:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {logout, logIn } from './store/authSlice'
+import { Header, Footer } from './components'
+import {Outlet} from 'react-router-dom'
+
+function App() {
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData) =>{
+      if(userData){
+        dispatch(logIn({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-500'>
+      <div className='w-full block' >
+         <Header/>
+        <main>
+         <Outlet/>
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : (<div>...Loading</div>)
+}
+
+export default App
---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import Header  from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from './Container/Container';
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Input from "./Input";
+import Button from './Button';
+import Select from './Select';
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from './Login';
+import PostCard from './PostCard';
+import PostForm from './PostForm/PostForm'
+import AuthLayout from './AuthLayout'
+export {
+    Header,
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    Input,
+    Button,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    PostCard,
+    PostForm,
+    AuthLayout
+}
